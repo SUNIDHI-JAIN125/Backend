bcb6c78c0dacee8151bac7d0882d22452f70e7bc
Sunidhi Jain <130484301+SUNIDHI-JAIN125@users.noreply.github.com>
2024-01-06 22:01:29 +0530

    Update ProductRoute.js

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -9,7 +9,7 @@ const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
 router.route("/").get(getAllProducts);
 router
-  .route("/admin/product/new")
+  .route("/product/new")
   .post(isAuthenticatedUser, createProduct);
   router.route("/product/:id").get(getProductDetails);
 
