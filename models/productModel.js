4961faeb61ba67094b49463137007d7c4de6781c
Sunidhi Jain <130484301+SUNIDHI-JAIN125@users.noreply.github.com>
2024-01-07 09:06:07 +0530

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -15,9 +15,11 @@ const productSchema = new mongoose.Schema({
     {
       public_id: {
         type: String,
+        required: true
       },
       url: {
         type: String,
+        required: true
       },
     },
   ],
